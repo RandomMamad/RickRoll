984f2666da4acf75cdbef94a973ac6b719501077
TheMamad <86688931+The-MaMad@users.noreply.github.com>
2022-03-28 17:49:11 +0430

    Switching from Bootstrap to JQuery and AnimeJS
    + Ripple effect on DiscordBtn
    + Linear Responsibility
    + Changing the icon of discord to text "Our Discord"
    + Cleaning Codes

---
+++ script.js
@@ -0,0 +1,9 @@
+var Discord;
+
+
+document.getElementById("DiscordBtn").onclick = DiscordOpenWindow = () =>
+  Discord = window.open(
+    "https://discord.gg/fZJZGs2",
+    "DescriptiveWindowName",
+    "left=512, top=64, width=512, height=512"
+  );
