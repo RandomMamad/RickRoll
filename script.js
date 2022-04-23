0e5d276a916a8bd439b4ffffa6e77ced2112a1c2
TheMamad <TGamerM9@gmail.com>
2022-04-23 19:23:42 +0430

    Making it a little better
    - deleting extra frameworks

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ document.getElementById("DiscordBtn").onclick = DiscordOpenWindow = () =>
     "https://discord.gg/fZJZGs2",
     "DescriptiveWindowName",
     "left=512, top=64, width=512, height=512"
-  );
+);
+
+
+if (1 > 0) {
+  this.player.muted = false; 
+}
